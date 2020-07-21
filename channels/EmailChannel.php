0b03b64b17c3fc5deff40366295178ce3b6b33c5
Soares e Neves <soareseneves@terra.com.br>
2020-07-21 15:29:13 -0300

    New fix

--- channels/EmailChannel.php
+++ channels/EmailChannel.php
@@ -57,10 +57,10 @@ class EmailChannel extends Channel
 
                 if (isset($emailParams)){
                     if (is_array($emailParams)){
-                        $emailParams = array_merge($emailParams, ['toUser' => $toUser, 'fromUser' => $fromUser, 'content' => $notification->getContent()]);
+                        $emailParams = array_merge($emailParams, ['toUser' => $toUser, 'fromUser' => $fromUser, 'data' => $notificationData]);
                     }
                 } else {
-                    $emailParams = ['toUser' => $toUser, 'fromUser' => $fromUser, 'content' => $notification->getContent()];
+                    $emailParams = ['toUser' => $toUser, 'fromUser' => $fromUser, 'data' => $notificationData];
                 }
 
                 $message = $this->mailer->compose(['html' => '@app/views/notifications/mail/' . $notification->getEmailTemplate(), 'text' => '@app/views/notifications/mail/text/' . $notification->getEmailTemplate()], $emailParams);
